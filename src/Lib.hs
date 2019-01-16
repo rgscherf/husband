4e1a642820d87b650b32320ea94f9c2565107479
rgscherf <rgscherf@gmail.com>
2019-01-16 13:14:43 -0800

    add constraing locking parsing and logic

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,14 +17,18 @@ data Expr
     | Dec
     | Loop [Expr]
     | Print
+    | LockInc
+    | LockDec
     deriving (Show)
 
-inc, dec, right, left, loop, prn :: Parser Expr
+inc, dec, right, left, loop, prn, lockInc, lockDec :: Parser Expr
 inc = char '+' >> return Inc
 dec = char '-' >> return Dec
 right = char '>' >> return MvRight
 left = char '<' >> return MvLeft
 prn = char '.' >> return Print
+lockInc = char '^' >> return LockInc
+lockDec = char 'v' >> return LockDec
 loop = do
     char '['
     exs <- many1 exprP
@@ -32,7 +36,7 @@ loop = do
     return $ Loop exs
 
 exprP :: Parser Expr
-exprP = loop <|> inc <|> dec <|> right <|> left <|> prn
+exprP = loop <|> inc <|> dec <|> right <|> left <|> prn <|> lockInc <|> lockDec
 
 exprs :: Parser [Expr]
 exprs = many exprP
@@ -57,32 +61,90 @@ modPtr op other ptr =
 incPtr = modPtr succ (-arrayBound)
 decPtr = modPtr pred arrayBound
 
+mkModErr :: String -> Int -> String -> String
+mkModErr triedTo ptrPos markedAs =
+    "Error. Tried to "
+        ++ triedTo
+        ++ " pointer at position "
+        ++ show ptrPos
+        ++ " but location was marked as "
+        ++ markedAs
+        ++ " only."
+
 -- Modifying tape values
 modCell
-    :: (Int -> Int -> Int)
+    :: CellOp
     -> (Array Int Int)
     -> Int
     -> ConstraintMap
-    -> (Array Int Int)
-modCell op arr ptr cons = case Data.Map.lookup ptr cons of
-    Just NoCons -> arr // [(ptr, (op) (arr ! ptr) 1)]
-    _           -> arr
+    -> Either String (Array Int Int)
+modCell op arr ptr cons
+    = let
+          opFn             = if op == IncOp then (+) else (-)
+          arrWithOpApplied = arr // [(ptr, (opFn) (arr ! ptr) 1)]
+      in
+          case Data.Map.lookup ptr cons of
+              Just NoCons  -> Right $ arrWithOpApplied
+              Just OnlyDec -> if op == DecOp
+                  then Right arrWithOpApplied
+                  else Left $ mkModErr "increment" ptr "decrement"
+              Just OnlyInc -> if op == IncOp
+                  then Right arrWithOpApplied
+                  else Left $ mkModErr "decrement" ptr "increment"
+              _ ->
+                  Left
+                      $  "An erorr occurred. Index "
+                      ++ show ptr
+                      ++ " not found on the tape."
 
-incCell = modCell (+)
-decCell = modCell (-)
+tryModCell :: CellOp -> Tape -> Either String Tape
+tryModCell op (Tape arr ptr pq cons) = case modCell op arr ptr cons of
+    Right arr' -> Right $ Tape arr' ptr pq cons
+    Left  s    -> Left s
 
-evalExprs :: [Expr] -> Tape -> Tape
-evalExprs []       tape                   = tape
-evalExprs (e : es) (Tape arr ptr pq cons) = case e of
-    Print ->
-        evalExprs es (Tape arr ptr ((chr $ mod (arr ! ptr) 128) : pq) cons)
-    Inc        -> evalExprs es (Tape (incCell arr ptr cons) ptr pq cons)
-    Dec        -> evalExprs es (Tape (decCell arr ptr cons) ptr pq cons)
-    MvRight    -> evalExprs es (Tape arr (incPtr ptr) pq cons)
-    MvLeft     -> evalExprs es (Tape arr (decPtr ptr) pq cons)
+writeCons :: ConstraintMap -> Int -> CellCons -> ConstraintMap
+writeCons cons key val =
+    if val == NoCons then cons else update (\v -> Just val) key cons
+
+tryWriteCons :: ConstraintMap -> Int -> CellCons -> Either String ConstraintMap
+tryWriteCons cons key val = case Data.Map.lookup key cons of
+    Just NoCons -> Right $ writeCons cons key val
+    Nothing ->
+        Left
+            $  "Tried to write new constraint for pointer value "
+            ++ show key
+            ++ ", but was not found."
+    Just c ->
+        Left
+            $  "Tried to write new constraint for pointer value "
+            ++ show key
+            ++ ", but constraint "
+            ++ show c
+            ++ " was already set."
+
+wrapTryWriteCons lockVal (Tape arr ptr pq cons) =
+    case tryWriteCons cons ptr lockVal of
+        Left  s -> Left s
+        Right c -> Right $ Tape arr ptr pq c
+
+-- try to evaluate exprs.
+-- evalution can fail iff user tries to modify a tape cell with a previously-imposed constraint.
+evalExprs :: [Expr] -> Either String Tape -> Either String Tape
+evalExprs []       tape                           = tape
+evalExprs _        (Left  s                     ) = Left s
+evalExprs (e : es) (Right (Tape arr ptr pq cons)) = case e of
+    Print -> evalExprs es
+        $ Right (Tape arr ptr ((chr $ mod (arr ! ptr) 128) : pq) cons)
+    Inc        -> evalExprs es $ tryModCell IncOp (Tape arr ptr pq cons)
+    Dec        -> evalExprs es $ tryModCell DecOp (Tape arr ptr pq cons)
+    MvRight    -> evalExprs es $ Right (Tape arr (incPtr ptr) pq cons)
+    MvLeft     -> evalExprs es $ Right (Tape arr (decPtr ptr) pq cons)
+    LockInc    -> evalExprs es $ wrapTryWriteCons OnlyInc $ Tape arr ptr pq cons
+    LockDec    -> evalExprs es $ wrapTryWriteCons OnlyDec $ Tape arr ptr pq cons
     Loop exprs -> case arr ! ptr of
-        0 -> evalExprs es $ Tape arr ptr pq cons
-        _ -> evalExprs (e : es) $ evalExprs exprs (Tape arr ptr pq cons)
+        0 -> evalExprs es $ Right $ Tape arr ptr pq cons
+        _ ->
+            evalExprs (e : es) $ evalExprs exprs $ Right (Tape arr ptr pq cons)
 
 regularParse :: Parser a -> String -> Either ParseError a
 regularParse p = parse p ""
@@ -94,16 +156,22 @@ strToExprs s = case regularParse exprs s of
 type HusbandResult = ([Char], [Int])
 eval :: String -> HusbandResult
 eval s =
-    let exprs             = strToExprs s
-        tape              = Tape initArr 0 [] initConstraintMap
-        (Tape arr _ pq _) = evalExprs exprs tape
-    in  (reverse pq, elems arr)
+    let exprs = strToExprs s
+        tape  = Right $ Tape initArr 0 [] initConstraintMap
+    in  case evalExprs exprs tape of
+            Right (Tape arr _ pq _) -> (reverse pq, elems arr)
+            Left  s                 -> (s, [])
+
+data CellOp
+    = IncOp
+    | DecOp
+    deriving (Eq)
 
 data CellCons
     = OnlyInc
     | OnlyDec
     | NoCons
-    deriving (Show)
+    deriving (Eq, Show)
 
 type ConstraintMap = Map Int CellCons
 
